375ee1f860f76b934761cbeae8909e6a25cc2258
João Vitor Pires Vilela de Oliveira <joao_pires@id.uff.br>
2024-05-15 18:17:18 -0300

    testa aí herói

--- main.py
+++ main.py
@@ -1,6 +1,11 @@
+from pygame.surface import Surface
+from pplay.window import Window
+from pplay.gameimage import GameImage
+from pytmx import load_pygame, TiledMap
+from pygame.surface import Surface
 import pygame
-from pplay import *
-import pytmx
+
+pygame.init()
 
 
 
@@ -16,10 +21,7 @@ import pytmx
 
 
 
-
-
-
-
+janela = pygame.display.set_mode((1920,1080))
 
 
 
@@ -27,3 +29,9 @@ import pytmx
 
 if __name__ == "__main__":
     print("Hanuman")
+    tmxdata = load_pygame('assets/testes.tmx')
+    chao = tmxdata.get_tile_image(0,0, layer=0)
+    parede = tmxdata.get_tile_image(0,0, layer=1)
+    while True:
+        janela.blit(chao, (0,0))
+        #janela.blit(parede, (0,0))
