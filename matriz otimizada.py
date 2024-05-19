8bcf82dbfc304861d33dd6a68c2ec6502e8b0746
Lucas Avelar <77032122+1lit4@users.noreply.github.com>
2024-05-19 11:38:46 -0300

    A MATRIZ FUNCIONA TÁ TUDO LINDO

--- matriz otimizada.py
+++ matriz otimizada.py
@@ -1,25 +1,43 @@
+#Voltei pra essa versão, essa tá limitada a só matriz quadrada (e é mais restrita) (restricao do quadrado menor)
+# (resolvi essa restricao mas deixei pq gostei dela)
+#desisti da forma recusiva
+#testei pra 1000 casos e n teve NENHUM erro (pode ser que eu tenha feito o teste errado tb né kkkk, mas acho q n)
+
 import random
-###
-### VERSAO BETA
-###
-def printar_matriz_dupla(matriz, m2=[]):
+
+def printar_matriz(matriz):
+    print('\033[0m')
     for l in range(len(matriz)):
         for c in range(len(matriz[l])):
-            print(f'{matriz[l][c]:2}', end=' ')
+            elem = matriz[l][c]
 
+            if elem == 3:
+                elem = f'\033[1;31m{elem:2}\033[0m'
+            elif elem == 2:
+                elem = f'\033[1;33m{elem:2}\033[0m'
+            elif elem == 1:
+                elem = f'\033[1;32m{elem:2}\033[0m'
+            else:
+                elem = f'\033[1;30m{elem:2}\033[0m'
+
+            print(f'{elem}', end=' ')
         
-        if m2 != []:
-            print('    ', end='')
-            for c in range(len(matriz[l])):
-                print(f'{m2[l][c]:2}', end=' ')
-            
+        print()
+    print()
+
+def printar_matriz_2(matriz):
+    print('\033[0m')
+    for l in range(len(matriz)):
+        for c in range(len(matriz[l])):
+            elem = matriz[l][c]
+            print(f'{elem:2}', end=' ')
+        
         print()
     print()
 
 def get_arround_list(matriz:list, index:tuple):
     '''Retorna o que tem em volta'''
-    resultado = [] #cima, baixo, direita, esquerda
-    emvolta = []
+
     linha = index[0]
     coluna = index[1]
 
@@ -28,125 +46,152 @@ def get_arround_list(matriz:list, index:tuple):
     
     contador = 0
 
-    if linha != 0:
+    if 0 < linha <= max_l and 0 <= coluna <= max_c:
         consulta = matriz[linha-1][coluna]
-        resultado.append([linha-1,coluna]) ##superior
-        emvolta.append(consulta)
+        ##superior
         contador += 1 if consulta != -1 and consulta != 0 else 0
-    else:
-        resultado.append(0)
-        emvolta.append(-2)
+
         
-    if linha != max_l:
+    if 0 <= linha < max_l and 0 <= coluna <= max_c:
         consulta = matriz[linha+1][coluna]
-        resultado.append([linha+1,coluna]) ##inferior
-        emvolta.append(consulta)
+        ##inferior
         contador += 1 if consulta != -1 and consulta != 0 else 0
-    else:
-        resultado.append(0)
-        emvolta.append(-2)
 
-    if coluna != max_c:
+
+    if 0 <= linha <= max_l and 0 <= coluna < max_c:
         consulta = matriz[linha][coluna+1]
-        resultado.append([linha,coluna+1]) ##direita
-        emvolta.append(consulta)
+        ##direita
         contador += 1 if consulta != -1 and consulta != 0 else 0
-    else:
-        resultado.append(0)
-        emvolta.append(-2)
+
         
-    if coluna != 0:
+    if 0 <= linha <= max_l and 0 < coluna <= max_c:
         consulta = matriz[linha][coluna-1]
-        resultado.append([linha,coluna-1]) ##esquerda
-        emvolta.append(consulta)
+        ##esquerda
         contador += 1 if consulta != -1 and consulta != 0 else 0
-    else:
-        resultado.append(0)
-        emvolta.append(-2)
+
     
-    return resultado, contador, emvolta
+    return contador
 
-def validar_em_volta(list):
-    for elem in list:
-        if elem != 0 and elem != -1:
-            return True
-    return False
-
-def gerar_matriz(n, minsalas, maxsalas):
-    global matriz_num #DEBUG NOTE
-    def cont_emvolta(matriz_original, lista_Problemas:list,):
-        '''No final resulta uma matriz que tem quantas salas tem em volta dquele indice'''
+def __cont_emvolta__(matriz_original, lista_Problemas:list = [],):
+        '''No final resulta uma matriz que tem quantas salas tem em volta dquele indice
+        Essa funcao só ta aqui pra DEBUG'''
         #transfomrar no consertador logo?
         matriz_cont =[]
         linhas = len(matriz_original)
         colunas = len(matriz_original[0])
-
-        max_l = linhas - 1
-        max_c = colunas -1 
-        
         for linha in range(linhas):
             temp = []
             for coluna in range(colunas):
-                if matriz_original[linha][coluna] != 0:
-                    temp.append(get_arround_list(matriz_original, (linha, coluna))[1])
+                if matriz_original[linha][coluna] != 0 and matriz_original[linha][coluna] != -1:
+                    temp.append(get_arround_list(matriz_original, (linha, coluna)))
                 else:
                     temp.append(0)
             matriz_cont.append(temp)
         return matriz_cont
-            
-        
-        
+
+def colocar_outras_salas(matriz:list, salas:list, posiveisPosicoes:list):
+    #lembrando que da forma que construimos o codigo, as possiveis posicoes já vem randomizadas
+    for i, elem in enumerate(salas):
+        posciao = posiveisPosicoes[i]
+        matriz[posciao[0]][posciao[1]] = elem
+
+
+def posciao_valida(matriz, l, c, n):
+    if matriz[l][c] == 1: # se já tem coisa ali n vamos continuar
+        return False
     
-    def backtracking(matriz:list, index:tuple, selecionaveis, PBLM4:list):
-        nonlocal salastotais
-        resultado, quantos, emvolta = get_arround_list(matriz, index)
-        if salastotais == 0:
-            matriz[index[0]][index[1]] = 3 #ja comeca adicionando pelo boss
-            salastotais += 1
-        else:
-            selecionado = random.choices(selecionaveis)[0]
-            if validar_em_volta(emvolta):
-                matriz[index[0]][index[1]] = selecionado
-            else:
-                matriz[index[0]][index[1]] = 0
-            
-            if selecionado != 0:
-                salastotais += 1
-            
-        random.shuffle(resultado)
-        for lado in resultado:
-            if -1 not in emvolta:
-                continue
-
-            if lado != 0 and matriz[lado[0]][lado[1]] == -1:
-                backtracking(matriz, lado, selecionaveis, PBLM4)
-                #ta se fechando e não ta voltando            
-        
-        return matriz
-            
-        
-
     
-    ### Depois remover ####
-    assert 0 < minsalas < n**2 
-    assert minsalas <= maxsalas < n**2
-    #######################
+    direcoes = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+    
+    #for para contar quantos tem em volta (to verificando os em volta da posicao que quero adcionar)
+    for linha, coluna in direcoes:
+        atual_l = l + linha
+        atual_c = c + coluna
 
+        if get_arround_list(matriz, (atual_l, atual_c)) == 3: # se tem alguem em volta com 3 ligacoes n podemos por mais um em volta
+            return False
+ 
+    ############## TIRA TUDO ISSO SE QUISER A FOMACAO QUADRADO (essa formacao quadrado tem um efeito labirintico)
+    #Esses ifs todos é pra checar as condicoes necessárias
+          #Se tá dentro da matriz ainda        #acima                 #esquerda                  #cima esquerda
+    if (0 <= l-1 < n and 0 <= c-1 < n) and (matriz[l-1][c] == 1 and matriz[l][c-1] == 1) and matriz[l-1][c-1] == 1:
+        return False
+          #Se tá dentro da matriz ainda        #acima                  #direita                  #cima direita
+    elif (0 <= l-1 < n and 0 <= c+1 < n) and (matriz[l-1][c] == 1 and matriz[l][c+1] == 1) and matriz[l-1][c+1] == 1:
+        return False
+          #Se tá dentro da matriz ainda        #abaixo                #esquerda                  #baixo esquerda
+    elif (0 <= l+1 < n and 0 <= c-1 < n) and (matriz[l+1][c] == 1 and matriz[l][c-1] == 1) and matriz[l+1][c-1] == 1:
+        return False
+          #Se tá dentro da matriz ainda        #abaixo                 #direita                  #baixo direita
+    elif (0 <= l+1 < n and 0 <= c+1 < n) and (matriz[l+1][c] == 1 and matriz[l][c+1] == 1) and matriz[l+1][c+1] == 1:
+        return False
+    ##############    
+
+
+    return True
+
+def gerar_matriz(tamanho:int, qntd:int):
+    '''tamanho: tamanho da matriz
+    qntd: quantidade de salas'''
     matriz = []
     
-    problemas_Quatro = []
-    selecionaveis = [0, 1, 2]
-    salastotais = 0
+    #inicia matriz de 0's
+    for _ in range(tamanho):
+        matriz.append([0]*tamanho)
+    
+    #aleatoriza a posicao inicial
+    posicao_inicial_l = random.randint(0, tamanho-1)
+    posicao_inicial_c = random.randint(0, tamanho-1)
+    
+    #posciao já começa com 1
+    matriz[posicao_inicial_l][posicao_inicial_c] = 1
+    
+    #essa lista vai ser de todas as posicoes dos que foram colocados (todos que tem 1) (tb vai ser util pra colocar as outras salas dps, loja boss e etc)
+    posicao_de_todos = [[posicao_inicial_l, posicao_inicial_c]] #como se fosse um identifiador de cada elemento 
+    
+    
+    direcoes = [[-1, 0], [1, 0], [0, -1], [0, 1]]
+                #cima     #baixo  #esquer  #direita
+    
+    #esse for todo é pra ir adcionando os 1's
+    for _ in range(qntd - 1): #-1 pq a gnt já adcionou um ali
+        
+        random.shuffle(posicao_de_todos) #pra aleatorizar o elemento de partida atual
+        
+        adicionado = False
+        for pos in posicao_de_todos:
+            l = pos[0]
+            c = pos[1]
 
-    for _ in range(n):
-        matriz.append([-1]*n)
-    
-    matriz = backtracking(matriz, [3,2], selecionaveis, problemas_Quatro)
-    matriz_num = cont_emvolta(matriz, [])
-    
-    printar_matriz_dupla(matriz, matriz_num) ##debug
+            
+            random.shuffle(direcoes) #pra ser uma direcao aleatoria
+            
+            #para cada direção
+            for linha, coluna in direcoes:
+                atual_l = l + linha
+                atual_c = c + coluna
+
+                   #previne acessar fora da memoria                        #funcao pra validar nossas condicoes
+                if (0 <= atual_l < tamanho and 0 <= atual_c < tamanho) and posciao_valida(matriz, atual_l, atual_c, tamanho):
+                    matriz[atual_l][atual_c] = 1 #coloco 1 de novo na nova posicao
+                    
+                    #da append na posicao atual pra gnt poder voltar dps
+                    posicao_de_todos.append([atual_l, atual_c]) # isso tudo pra n fazer recursiva....
+                    
+                    adicionado = True
+                    break # se adcionei nao passo mais pelas direcoes (é pq eu atualizei a lista posicao_de_todos, preciso dela no meu for)
+
+            
+            if adicionado:
+                break
+        
+
+    colocar_outras_salas(matriz, [2,3], posicao_de_todos)  
     return matriz
-    
 
 
-gerar_matriz(6, 3, 4)
+n = 6 
+max = 29 
+matriz = gerar_matriz(n, max)
+printar_matriz(matriz)
+
