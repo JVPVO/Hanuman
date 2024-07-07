75845610e03a198d21b98ab5241a5c9abf91c0cb
Lucas Avelar <77032122+1lit4@users.noreply.github.com>
2024-07-07 11:54:17 -0300

    nova sala

--- assets/base/dungeon_props.tsx
+++ assets/base/dungeon_props.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.11.0" name="dungeon_props" tilewidth="48" tileheight="34" tilecount="10" columns="0">
+<tileset version="1.10" tiledversion="1.11.0" name="dungeon_props" tilewidth="64" tileheight="64" tilecount="46" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="0">
   <image source="../dungeon_props/dungeon_props_02.png" width="16" height="23"/>
@@ -30,5 +30,113 @@
  </tile>
  <tile id="9">
   <image source="../dungeon_props/dungeon_props_31.png" width="12" height="10"/>
+ </tile>
+ <tile id="10">
+  <image source="../dungeon_props/Tiles_30.png" width="14" height="13"/>
+ </tile>
+ <tile id="11">
+  <image source="../dungeon_props/Tiles_33.png" width="12" height="9"/>
+ </tile>
+ <tile id="12">
+  <image source="../dungeon_props/Tiles_35.png" width="16" height="26"/>
+ </tile>
+ <tile id="13">
+  <image source="../dungeon_props/Tiles_41.png" width="9" height="7"/>
+ </tile>
+ <tile id="14">
+  <image source="../dungeon_props/Tiles_45.png" width="8" height="6"/>
+ </tile>
+ <tile id="15">
+  <image source="../dungeon_props/Tiles_48.png" width="11" height="7"/>
+ </tile>
+ <tile id="16">
+  <image source="../dungeon_props/Tree_03.png" width="13" height="8"/>
+ </tile>
+ <tile id="17">
+  <image source="../dungeon_props/Tree_07.png" width="18" height="9"/>
+ </tile>
+ <tile id="18">
+  <image source="../dungeon_props/Tree_11.png" width="18" height="9"/>
+ </tile>
+ <tile id="19">
+  <image source="../dungeon_props/Tree_14.png" width="17" height="11"/>
+ </tile>
+ <tile id="20">
+  <image source="../dungeon_props/caslte_03.png" width="18" height="35"/>
+ </tile>
+ <tile id="21">
+  <image source="../dungeon_props/caslte_05.png" width="26" height="35"/>
+ </tile>
+ <tile id="22">
+  <image source="../dungeon_props/caslte_08.png" width="19" height="33"/>
+ </tile>
+ <tile id="23">
+  <image source="../dungeon_props/caslte_13.png" width="19" height="36"/>
+ </tile>
+ <tile id="24">
+  <image source="../dungeon_props/caslte_14.png" width="18" height="33"/>
+ </tile>
+ <tile id="25">
+  <image source="../dungeon_props/caslte_16.png" width="15" height="33"/>
+ </tile>
+ <tile id="26">
+  <image source="../dungeon_props/caslte_21.png" width="12" height="20"/>
+ </tile>
+ <tile id="27">
+  <image source="../dungeon_props/caslte_24.png" width="14" height="20"/>
+ </tile>
+ <tile id="28">
+  <image source="../dungeon_props/caslte_27.png" width="11" height="16"/>
+ </tile>
+ <tile id="29">
+  <image source="../dungeon_props/caslte_29.png" width="13" height="16"/>
+ </tile>
+ <tile id="30">
+  <image source="../dungeon_props/dungeon_03.png" width="18" height="64"/>
+ </tile>
+ <tile id="31">
+  <image source="../dungeon_props/forest_03.png" width="18" height="29"/>
+ </tile>
+ <tile id="32">
+  <image source="../dungeon_props/forest_06.png" width="15" height="27"/>
+ </tile>
+ <tile id="33">
+  <image source="../dungeon_props/forest_09.png" width="11" height="14"/>
+ </tile>
+ <tile id="34">
+  <image source="../dungeon_props/Tiles_03.png" width="44" height="41"/>
+ </tile>
+ <tile id="35">
+  <image source="../dungeon_props/Tiles_06.png" width="40" height="41"/>
+ </tile>
+ <tile id="36">
+  <image source="../dungeon_props/Tiles_10.png" width="38" height="37"/>
+ </tile>
+ <tile id="37">
+  <image source="../dungeon_props/Tiles_13.png" width="32" height="22"/>
+ </tile>
+ <tile id="38">
+  <image source="../dungeon_props/Tiles_17.png" width="32" height="47"/>
+ </tile>
+ <tile id="39">
+  <image source="../dungeon_props/Tiles_22.png" width="11" height="7"/>
+ </tile>
+ <tile id="40">
+  <image source="../dungeon_props/Tiles_26.png" width="9" height="9"/>
+ </tile>
+ <tile id="41">
+  <image source="../dungeon_props/teia_03.png" width="16" height="16"/>
+ </tile>
+ <tile id="42">
+  <image source="../dungeon_props/teia_05.png" width="16" height="16"/>
+ </tile>
+ <tile id="43">
+  <image source="../dungeon_props/teia_06.png" width="64" height="64"/>
+ </tile>
+ <tile id="44">
+  <image source="../dungeon_props/teia_10.png" width="16" height="16"/>
+ </tile>
+ <tile id="45">
+  <image source="../dungeon_props/teia_11.png" width="16" height="16"/>
  </tile>
 </tileset>
